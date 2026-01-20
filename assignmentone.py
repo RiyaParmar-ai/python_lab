fdd1a6b060498fc590ad87fad4ea610780f2bba6
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-01-20 09:24:10 +0530

    Add files via upload

---
+++ assignmentone.py
@@ -0,0 +1,39 @@
+#x="hello people"
+#print(x)
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result=float=num1+num2
+print("The sum is : ")
+num=int(input("enter a number:"))
+if num%2==0
+       print("the number is even")
+else
+    print("the number is odd")
+year=int(input("enter a year: "))   
+if year %4==0;
+   print("leap year")
+else
+   print("not a leap year") 
+PI=3.14
+print(PI)
+num=int(input("enter a number: "))
+print("square : ",num**2)
+radius=float(input("enter the radius of circle"))
+area=3.14*radius*radius
+print("area of circle: ",area)
+data=input("enter something")
+print("data type is: ",type(data))
+import math
+angle=90
+radians=math.radians(angle)
+print("sine of 90 degrees:{math.sin(radians)}")
+base=5
+exponent=3
+result=math.pow(base,expononet)
+printf("{base} to the power of {exponent} is {results}")
+if number>0:
+    print("positive")
+elif number<0;
+    print("negative")
+else:
+    print("zero")        
