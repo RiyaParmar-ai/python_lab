f69d8380b6039a5990387ef2bc9a6774ecb8d869
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-01-29 11:15:51 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,41 @@
+#1
+"""a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)"""
+#2
+"""x=35
+print(x)"""
+#3
+a=3
+print(-a)
+#4
+a=15
+b=20
+print(a>b)
+#5
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+#6
+is_pass=True
+print(is_pass)
+#7
+a=5
+b=3
+print(a and b)
+print(a/b)
+#8
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers)
+#9
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+
