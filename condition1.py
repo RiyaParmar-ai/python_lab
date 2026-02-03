5d605f65808876d56b124e788d6a0f4f57232bec
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-02-03 09:10:41 +0530

    Add files via upload

---
+++ condition1.py
@@ -0,0 +1,13 @@
+#1
+age = int(input("Enter your age: "))
+if age>=18:
+    print("Yes you're eligible for vote")
+else:
+    print("No you're not eligible for vote")
+
+#2
+marks = int(input("Enter your marks: "))
+if marks>=35:
+    print("PASS")
+else:
+    print("FAIL")           
