6f617d16528878d2a40fb8f5933ff64620f83312
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-02-24 08:37:21 +0530

    Add files via upload

---
+++ taskofwhileloop.py
@@ -0,0 +1,20 @@
+#1. Print number from 1 to 5
+"""i=1
+while i<=5:
+    print(i)
+    i=i+1"""
+
+#2. Sum of numbers.(Take user's input)
+"""n=int(input("Enter n : "))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("Sum = ",s)"""
+
+#3. Print Odd no. between 1 and 20
+num=1
+while num <= 20:
+    print(num)
+num+=2    
