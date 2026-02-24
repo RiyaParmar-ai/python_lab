84142f2f7cb95a2d13eb9063443c85b6f792e052
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-02-24 08:37:57 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,33 @@
+#1. Print Numbers from 1 to 10
+"""for i in range(1,11):
+    print(i)"""
+
+#2. Print Even Numbers from 1 to 20
+"""for i in range(1,21):
+    if i%2 == 0:
+        print(i)"""
+
+#3. Print Odd Numbers from 1 to 15
+"""for i in range(1,16):
+    if i%2 !=0:
+        print(i)""" 
+
+#4.Print Table of 5
+"""for i in range (1,11):
+    print("5 x",i, "=",5*i)"""
+
+#5.Print Characters of a string
+"""name="Melodious"
+for letter in name:
+    print(letter)"""
+
+#6.Sum of Numbers from 1 to 5
+"""total=0
+for i in range(1,6):
+    total=total+i
+    print("Sum is : ",total)"""
+
+#7.Print List Elements
+"""numbers =[10,20,30,40]
+for n in numbers:
+    print(n)"""
