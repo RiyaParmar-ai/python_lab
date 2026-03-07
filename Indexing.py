1cb83a880ac1a79841d1519949fb5223f591a893
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-03-07 08:50:30 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,21 @@
+#1. POSITIVE INDEXING
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+
+#2. NEGATIVE INDEXING
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+
+#3. MODIFYING ELEMENTS USING INDEX
+from array import array 
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
+
+
