cb95d6d59b1bf1d05681c87c16ac4fadb3ab8f36
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-07-07 09:03:16 +0530

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,19 @@
+#1.append()
+list=[10,20,30,40,50]
+list.append(60)
+print(list)
+
+#2.insert()
+list=[10,20,30,40,50]
+list.insert(2,25)
+print(list)
+
+#3.pop()
+list=[10,20,30,40,50]
+list.pop()
+print(list)
+
+#4.pop() using index
+list=[10,20,30,40,50]
+list.pop(3)
+print(list)
