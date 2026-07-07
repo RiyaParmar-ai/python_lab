6e7b48e07844ee05c0709d6537ba2adda2e85d6c
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-07-07 09:03:41 +0530

    Add files via upload

---
+++ rno.py
@@ -0,0 +1,7 @@
+import numpy as np
+r_no=np.array([10,20,30,40,50])
+subject=np.array(["Maths","Science","English","Sanskrit","Art"])
+marks=np.array([79,67,54,89,99,100])
+print(r_no)
+print(subject)
+print(marks)
