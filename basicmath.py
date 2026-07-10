3e70f4a2339ebe41ce48b9a8745ea5f6347c8fb7
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-07-10 09:14:09 +0530

    Add files via upload

---
+++ basicmath.py
@@ -0,0 +1,25 @@
+import sys
+import numpy as np
+#1. Addition
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+
+#2. Subtraction
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Subtraction =", a - b)
+
+#3.Multiplication
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("Multiplication =", a * b)
+
+#4. Division
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("Division =",a/b)
