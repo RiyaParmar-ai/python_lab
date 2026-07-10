d8b6f90804358d343fa315c9cb6df7eebeb95579
RiyaParmar-ai <riyaparmar17052006@gmail.com>
2026-07-10 09:14:25 +0530

    Add files via upload

---
+++ basicmathwitharr.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+
+#1. add
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([1, 2, 3])
+
+print("Addition =", arr1 + arr2)
+
+#2. subtract
+import numpy as np
+
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([1, 2, 3])
+
+print("Subtraction =", arr1 - arr2)
+
+#3. multiply
+arr1 = np.array([20])
+arr2 = np.array([2])
+
+print("Multiplication =", arr1 * arr2)
+
+#4. Divide 
+arr1 = np.array([20])
+arr2 = np.array([2])
+
+print("Division =", arr1 / arr2)
+
